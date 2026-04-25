1f782096b314f85f81812fc4f9823aa9fd604fce
poojahn4698 <poojahn4@gmail.com>
2026-04-25 23:10:55 +0530

    Two Sum-Leetcode-1

---
+++ src/TwoSum.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] sum = twoSum(nums,target);
+        for (int i:sum){
+            System.out.print(i +" ");
+        }
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            int constraint = target-nums[1];
+            if(map.containsKey(constraint)){
+                return new int[]{map.get(constraint),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
